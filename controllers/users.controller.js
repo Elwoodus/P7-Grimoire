698743c1f7300b488d3e078091d96927c183bc49
Benjamin Hautefaye <titouchef@hotmail.fr>
2023-06-23 19:11:48 +0200

    ajout feature des books

---
+++ controllers/users.controller.js
@@ -0,0 +1,75 @@
+const { User } = require("../models/User");
+const bcrypt = require("bcrypt");
+const express = require("express");
+
+async function signUp(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+
+    const userInDb = await User.findOne({
+        email: email
+    });
+
+    if (userInDb != null) {
+        res.status(400).send("Email already exists");
+        return;
+    }
+    const user = {
+        email: email,
+        password: hashPassword(password)
+    };
+    try {
+        await User.create(user);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send("Something went wrong");
+        return;
+    }
+
+    res.send("Sign Up");
+}
+
+async function login(req, res) {
+    const body = req.body;
+
+
+    const userInDb = await User.findOne({
+        email: body.email
+    });
+    if (userInDb == null) {
+        res.status(400).send("Wrong email");
+        return;
+    }
+
+    const passwordInDb = userInDb.password;
+    if (!isPasswordCorrect(req.body.password, passwordInDb)) {
+        res.status(400).send("Wrong password");
+        return;
+    }
+
+    res.send({
+        userId: userInDb._id,
+        token: "token"
+    });
+}
+
+function hashPassword(password) {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    return hash;
+}
+
+function isPasswordCorrect(password, hash) {
+    return bcrypt.compareSync(password, hash);
+
+}
+
+
+const usersRouter = express.Router();
+
+usersRouter.post("signup", signUp);
+usersRouter.post("login", login);
+
+
+module.exports = { usersRouter }
--- index.js
+++ index.js
@@ -1,118 +1,18 @@
-const express = require("express");
-const app = express();
-const {User} = require("./db/mongo");
-const cors = require("cors");
-const bcrypt = require("bcrypt");
-const {books} = require("./db/books");
-const multer = require("multer");
+const {app} = require("./config/app");
+const { usersRouter } = require("./controllers/users.controller");
+const { booksRouter } = require("./controllers/books.controller");
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads");
-    },
-    filename: function (req, file, cb) {
-        const fileName = file.originalname.toLowerCase() + Date.now() + ".jpg";
-        cb(null, Date.now() + "-" + fileName);
-    }
-})
-
-const upload = multer ({
-    storage: storage
-})
-
-const PORT = 4000;
-
-app.use(cors());
-app.use(express.json());
-
-
-function sayHi(req, res) {
-    res.send("Hello World");
-}
-
-app.get("/", sayHi);
-app.post("/api/auth/signup", signUp);
-app.post("/api/auth/login", login);
-app.get("/api/books", getBooks);
-app.post("/api/books", upload.single("image"), postBook);
-
-
-function postBook(req, res) {
-    const book = req.body;
-}
-
-function getBooks(req, res){
-    res.send(books);
-}
-
-app.listen(PORT, function () {
-    console.log(`Server is running on : ${PORT}`);
-});
+app.get("/", (req,res) => res.send ("Server running!"));
 
 
 
-async function signUp(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
-    
-
-    const userInDb = await User.findOne({
-        email : email
-    });
-    
-    if (userInDb != null) {
-        res.status(400).send("Email already exists");
-        return;
-    }
-    const user = {
-        email: email,
-        password: hashPassword(password)
-    };
-    try {
-        await User.create(user);
-        } catch (e) {
-        console.error(e);
-        res.status(500).send("Something went wrong");
-        return;
-    }
-     
-    res.send("Sign Up");
-}
+app.use("/api/auth", usersRouter);
+app.use("/api/books", booksRouter);
 
 
 
-async function login(req, res) {
-    const body = req.body;
 
 
-    const userInDb = await User.findOne({
-        email : body.email
-    });
-    if (userInDb == null) {
-        res.status(400).send("Wrong email");
-        return;
-    }
 
-    const passwordInDb = userInDb.password;
-    if (!isPasswordCorrect(req.body.password, passwordInDb)) {
-        res.status(400).send("Wrong password");
-        return;
-    }
 
-    res.send({
-        userId: userInDb._id,
-        token: "token"
-    });
-}
-
-function hashPassword(password) {
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(password, salt);
-    return hash;
-}
-
-function isPasswordCorrect(password, hash) {
-return bcrypt.compareSync(password, hash);
-
-}
 
